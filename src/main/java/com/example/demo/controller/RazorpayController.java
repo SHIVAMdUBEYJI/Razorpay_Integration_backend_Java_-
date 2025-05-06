f5e7ed2523c23bb411542841a1f6193191cc3b69
shivam dubey <youremail@example.com>
2025-05-06 13:33:16 +0530

    removed the slash in RazorpayController class

--- src/main/java/com/example/demo/controller/RazorpayController.java
+++ src/main/java/com/example/demo/controller/RazorpayController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RazorpayController {
 
     @Autowired
